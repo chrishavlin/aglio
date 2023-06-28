de2ac16815c2e63b561278984fe95b0105b6b143
chrishavlin <chris.havlin@gmail.com>
2023-06-28 16:29:28 -0500

    updating examples (they run, need more narrative)

--- examples/load_netcdf.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-import aglio
-
-filename = "IRIS/GYPSUM_percent.nc"
-
-ds = aglio.open_dataset(filename)
-ds.aglio.surface_gpd.plot()
-plt.show()
--- examples/velocity_perturbations.py
+++
@@ -1,9 +0,0 @@
-import aglio
-from aglio.seismology import datasets as ysd
-
-filename = "IRIS/wUS-SH-2010_percent.nc"
-refmodel = "IRIS/refModels/AK135F_AVG.csv"
-
-refAK = ysd.load_1d_csv_ref_collection(refmodel, "depth_km")
-ds = aglio.open_dataset(filename)
-abs_vs = ds.aglio.get_absolute(refAK, "dvs", "Vs_kms")
