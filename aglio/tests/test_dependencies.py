0415788ee46312062b7d29b4601c9a145b3acc2e
chrishavlin <chris.havlin@gmail.com>
2023-12-01 12:15:43 -0600

    add a test

--- aglio/tests/test_dependencies.py
+++ aglio/tests/test_dependencies.py
@@ -1,11 +1,12 @@
 import pytest
 
-from aglio._utilities.dependencies import dependency_checker
+from aglio._utilities.dependencies import TimeSeriesKMeansDummy, dependency_checker
 
 
 def test_attributes():
-    assert dependency_checker.has_yt is True
+    assert dependency_checker.has_yt
     assert dependency_checker.has_cartopy is False
+    assert dependency_checker.has_tslearn
 
 
 def test_decorator():
@@ -18,3 +19,8 @@ def test_decorator():
     missing_wrapped = dependency_checker.requires("not_a_module", temp_func)
     with pytest.raises(ImportError, match="This method requires not_a_module"):
         _ = missing_wrapped(2)
+
+
+def test_tslearn_dummy():
+    with pytest.raises(ImportError, match="This functionality requires tslearn"):
+        _ = TimeSeriesKMeansDummy()
